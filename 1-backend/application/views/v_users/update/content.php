6354a01c390e9c67d05b8515832f2e0c9be1323c
Omer Kavak <omerkvak0@gmail.com>
2024-05-09 13:48:12 +0300

    Users ödevi yapıldı.

--- 1-backend/application/views/v_users/update/content.php
+++ 1-backend/application/views/v_users/update/content.php
@@ -23,13 +23,40 @@
                 <form class="form-horizontal" method="POST" action="<?php echo base_url("Users/update/$item->id") ?>">
                   <div class="card-body">
                     <div class="form-group row">
-                      <label for="title" class="col-sm-2 col-form-label">Ürün Markası:</label>
+                    <label for="title" class="col-sm-2 col-form-label">E-Posta:</label>
                       <div class="col-sm-10">
 
-                        <input type="text" name="title" class="form-control" id="title" value="<?php echo isset($formError) ? set_value("title") : $item->title; ?>" placeholder="Ürün Markasını Giriniz.">
+                        <input type="text" name="email" class="form-control" id="email" value="<?php echo isset($formError) ? set_value("email") : $item->email; ?>" placeholder="E-Posta Giriniz.">
 
                         <?php if (isset($formError)) { ?>
-                          <small><?php echo form_error("title"); ?></small>
+                          <small><?php echo form_error("email"); ?></small>
+                        <?php } ?>
+                      </div><br><br>
+                      <label for="title" class="col-sm-2 col-form-label">İsim:</label>
+                      <div class="col-sm-10">
+
+                        <input type="text" name="name" class="form-control" id="name" value="<?php echo isset($formError) ? set_value("name") : $item->name; ?>" placeholder="İsim Giriniz.">
+
+                        <?php if (isset($formError)) { ?>
+                          <small><?php echo form_error("name"); ?></small>
+                        <?php } ?>
+                      </div>  <br><br>
+                      <label for="title" class="col-sm-2 col-form-label">Soyisim:</label>
+                      <div class="col-sm-10">
+
+                        <input type="text" name="surname" class="form-control" id="surname" value="<?php echo isset($formError) ? set_value("surname") : $item->surname; ?>" placeholder="Soyisim Giriniz.">
+
+                        <?php if (isset($formError)) { ?>
+                          <small><?php echo form_error("surname"); ?></small>
+                        <?php } ?>
+                      </div><br><br>
+                      <label for="title" class="col-sm-2 col-form-label">Şifre:</label>
+                      <div class="col-sm-10">
+
+                        <input type="text" name="password" class="form-control" id="password" value="<?php echo isset($formError) ? set_value("password") : $item->password; ?>">
+
+                        <?php if (isset($formError)) { ?>
+                          <small><?php echo form_error("password"); ?></small>
                         <?php } ?>
                       </div>
                     </div>
